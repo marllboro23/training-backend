3f7ad814d130c9fb8ceca176cfba247b55363afa
marllboro23 <donaldnam1995@gmail.com>
2020-03-07 13:54:39 +0700

    update API

--- index.js
+++ index.js
@@ -1,19 +1,91 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require("cors")
 
 const app = express()
+app.use(cors())
+let id = 0
+
+app.use(bodyParser.json())
+
+let dataUser = require('./data.json')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-    console.log(__dirname)
-    res.send(express.static(__dirname + '/public'))
+    res.send(dataUser.users)
 })
 
-app.post("/register", (req, res) => {
-    console.log(req.body)
-    res.send("success")
+// Sigin and Register
+app.post("/register", async (req, res) => {
+    let { username, password, email } = req.body
+    bcrypt.hash(password, null, null, (err, hash) => {
+        password = hash
+    })
+
+    dataUser.users.push({
+        id: id++,
+        username: username,
+        password: password,
+        email: email
+    })
+    res.json(dataUser.users[dataUser.users.length - 1])
 })
 
-app.listen(8080)
+app.post('/sigin', (req, res, next) => {
+    const { body } = req
+
+    // check password is true or false
+    bcrypt.compare(
+        "cookies",
+        '$2a$10$TQ4WVWoBcIenW5ovnLLyxOO3oXaDg5GyJbKueCZQIwg1RbN7S5mGG',
+        (err, res) => {
+            console.log(res)
+        })
+
+    dataUser.users.map(user => {
+        if (user.username === body.username && user.password === body.password) {
+            console.log(1)
+            return res.send({
+                success: 1,
+                message: "success",
+                status: true
+            })
+            // next()
+        }
+        else {
+            return res.status(400).json({
+                success: 0,
+                message: "not success",
+                status: false
+            })
+        }
+
+    })
+})
+
+// Get profile user
+app.get("/profile", (req, res) => {
+    const { id } = req.query
+
+    const findResult = dataUser.users.find(user => user.id = id)
+    res.send(findResult)
+})
+
+app.get("/profile/:id", (req, res) => {
+    const { id } = req.params
+
+    const findUser = dataUser.users.filter(user => user.id === id)
+    res.send(findUser)
+})
+
+// Get all user 
+app.get('/all-user', (req, res) => {
+    res.send(dataUser.users)
+})
+
+app.listen(8080, () => {
+    console.log("Server is running on port 8080")
+})
